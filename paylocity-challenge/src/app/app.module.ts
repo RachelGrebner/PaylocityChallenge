3cf75a170c8dc98a3b43e4b9d93ce8c2cf939fa3
Rachel Grebner <rachelannegrebner@gmail.com>
2020-07-12 12:51:04 -0400

    adding employee detail component

--- paylocity-challenge/src/app/app.module.ts
+++ paylocity-challenge/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employees/employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule,
