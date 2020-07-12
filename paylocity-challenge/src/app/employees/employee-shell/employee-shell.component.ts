9f9392714fe1127571a745975b2ea95226a7a1ad
Rachel Grebner <rachelannegrebner@gmail.com>
2020-07-12 14:01:06 -0400

    WIP

--- paylocity-challenge/src/app/employees/employee-shell/employee-shell.component.ts
+++ paylocity-challenge/src/app/employees/employee-shell/employee-shell.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators, FormArray } from "@angular/forms";
 
 @Component({
   selector: "app-employee-shell",
@@ -7,12 +7,35 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class EmployeeShellComponent implements OnInit {
   employeeForm: FormGroup;
+  employeeBenefitCost: string;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.employeeForm = this.fb.group({
-      name: ["", Validators.required],
+      employeeName: ["", Validators.required],
+      dependents: this.fb.array([]),
     });
+  }
+
+  onFormSubmit() {
+    this.employeeBenefitCost = "I Have Submitted the Form";
+  }
+
+  addDependent() {
+    (<FormArray>this.employeeForm.get("dependents")).push(
+      this.fb.group({
+        //dependentName: ["", Validators.required],
+        dependentName: [],
+      })
+    );
+  }
+
+  get dependents() {
+    return (<FormArray>this.employeeForm.get("dependents")).controls;
+  }
+
+  removeDependent(i) {
+    (<FormArray>this.employeeForm.get("dependents")).removeAt(i);
   }
 }
