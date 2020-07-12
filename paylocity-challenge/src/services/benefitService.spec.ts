7ce57354e4d08c63648f15973e6fbc9e5e9093c1
Rachel Grebner <rachelannegrebner@gmail.com>
2020-07-12 14:41:15 -0400

    should discount appropriately if employee name begins with A but dependents dont

--- paylocity-challenge/src/services/benefitService.spec.ts
+++ paylocity-challenge/src/services/benefitService.spec.ts
@@ -29,7 +29,7 @@ describe("benefitService", () => {
     expect(benefitCost).toBe(900);
   });
 
-  it("should discount employee benefit appropriately if employee has dependents whose names begin with A", () => {
+  it("should discount employee benefit appropriately if employee does not have a name that starts with A, but has dependents whose names begin with A", () => {
     let employeeName = "Ben";
     let dependentNames = ["Andrea", "Amanda"];
     let benefitCost = benefitService.calculateBenefitCost(
@@ -38,5 +38,16 @@ describe("benefitService", () => {
     );
 
     expect(benefitCost).toBe(1900);
+  });
+
+  it("should discount employee benefit appropriately if employee does have a name that starts with A, but has dependents names do not start with A", () => {
+    let employeeName = "Adam";
+    let dependentNames = ["Sally", "Ryan", "Tom", "Tim"];
+    let benefitCost = benefitService.calculateBenefitCost(
+      employeeName,
+      dependentNames
+    );
+
+    expect(benefitCost).toBe(2900);
   });
 });
