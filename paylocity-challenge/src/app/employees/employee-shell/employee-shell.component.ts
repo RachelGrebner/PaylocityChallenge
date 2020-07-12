e8f3b794d4ab2663be8f3ed1a0a8de2a7bdc20dc
Rachel Grebner <rachelannegrebner@gmail.com>
2020-07-12 13:30:10 -0400

    removing uneccesary components, adding preliminary part of form

--- paylocity-challenge/src/app/employees/employee-shell/employee-shell.component.ts
+++ paylocity-challenge/src/app/employees/employee-shell/employee-shell.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-employee-shell",
   templateUrl: "./employee-shell.component.html",
 })
 export class EmployeeShellComponent implements OnInit {
-  constructor() {}
+  employeeForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.employeeForm = this.fb.group({
+      name: ["", Validators.required],
+    });
+  }
 }
--- paylocity-challenge/src/app/employees/employee.module.ts
+++ paylocity-challenge/src/app/employees/employee.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
 
-import { EmployeeListComponent } from "./employee-list/employee-list.component";
 import { EmployeeShellComponent } from "./employee-shell/employee-shell.component";
-import { EmployeeDetailComponent } from "./employee-detail/employee-detail.component";
 
 import { SharedModule } from "../shared/shared.module";
 
@@ -19,10 +17,6 @@ import { SharedModule } from "../shared/shared.module";
       },
     ]),
   ],
-  declarations: [
-    EmployeeListComponent,
-    EmployeeShellComponent,
-    EmployeeDetailComponent,
-  ],
+  declarations: [EmployeeShellComponent],
 })
 export class EmployeeModule {}
